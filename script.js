3828207be9495b2545658b82836ddbc9e5441c51
Vishal Brijesh kanojiya <90042549+kanojiya9768@users.noreply.github.com>
2022-10-11 17:28:08 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,25 @@
+let green = () =>{
+  document.documentElement.style.setProperty('--bg_color','#00ff48');
+}
+
+let pink = () =>{
+  document.documentElement.style.setProperty('--bg_color','#ff0062');
+}
+
+let violet = () =>{
+  document.documentElement.style.setProperty('--bg_color','#ff00cc');
+}
+
+let yellow = () =>{
+  document.documentElement.style.setProperty('--bg_color','#fff200');
+  document.documentElement.style.setProperty('--text_color','#000');
+}
+
+let orange = () =>{
+  document.documentElement.style.setProperty('--bg_color','#ff6a00');
+}
+
+let black = () =>{
+  document.documentElement.style.setProperty('--bg_color','#000');
+   document.documentElement.style.setProperty('--text_color','#fff');
+}
